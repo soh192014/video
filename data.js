fedcc7b2ec09b87d7415b8ccad853cba9aba7051
soh_19204 <m-13697019@moe-dl.edu.my>
2026-06-28 13:05:47 +0800

    Add video data to data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+const videos = [
+{
+    title: "Epic Cinematic",
+    id: "1oB_z3em4Nf0d6Wqq8qgxPyMOUNwBV6ph",
+    cover: "https://picsum.photos/400/225?1",
+    desc: "Netflix 风格演示视频"
+},
+{
+    title: "Nature Relax",
+    id: "1oB_z3em4Nf0d6Wqq8qgxPyMOUNwBV6ph",
+    cover: "https://picsum.photos/400/225?2",
+    desc: "自然放松视频"
+}
+];
